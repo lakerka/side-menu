d1fcca92f5369a7e2c24dcf113960209f0860009
Žilvinas Rudžionis <zilvinas.rudzionis@adform.com>
2017-12-25 12:48:24 +0200

    Make parent field readonly after object creation.

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from products.models import Category, Product
 
-admin.site.register([Category, Product])
+
+class CategoryAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        read_only_field = []
+        if obj:
+            read_only_field = ['parent']
+        return read_only_field
+
+
+class ProductAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        read_only_field = []
+        if obj:
+            read_only_field = ['parent_category']
+        return read_only_field
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
